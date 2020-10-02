140cbd6df2e90551cdeeb52d1b8e4adf413f88f9
Thulasirobocop <35140678+Thulasirobocop@users.noreply.github.com>
2020-10-02 12:57:44 +0530

    Add files via upload

---
+++ Face detection live/cam.py
@@ -0,0 +1,31 @@
+# import the opencv library 
+import cv2 
+import numpy as np
+cascade=cv2.CascadeClassifier('D:\\PROJECT\\Face detection live\\haarcascade_frontalface_default.xml')
+  
+# define a video capture object 
+vid = cv2.VideoCapture(0)
+vid_cod = cv2.VideoWriter_fourcc(*'XVID')
+output = cv2.VideoWriter("C:\\Users\\sthul\\Downloads\\cam_video.mp4", vid_cod, 20.0, (640,480))
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read() 
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    eyes=cascade.detectMultiScale(gray,1.3,5)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,0,255),3)
+    # Display the resulting frame 
+    cv2.imshow('frame', frame) 
+    output.write(frame)  
+    # the 'q' button is set as the 
+    # quitting button you may use any 
+    # desired button of your choice 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
+
